6403c0001b9c313ebc16592717bb5812239659b9
IAM-Rana-Ahmad <r80228927@gmail.com>
2024-01-11 20:50:10 +0500

    changing in the backend server

--- client/crud-app/src/services/postData.jsx
+++ client/crud-app/src/services/postData.jsx
@@ -1,9 +1,10 @@
 import axios from "axios"
 
-const URL=""
 export const addData=async(data)=>{
+
+const URL="http://localhost:5000/"
     try {
-      await  axios.get(URL,data)
+      await  axios.get(`${URL}/add`,data)
     } catch (error) {
         console.log("Unknown error is occured")
     }
